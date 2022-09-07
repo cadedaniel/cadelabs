d2855ad340109a2c78f0deb84a4f0eb4a07fe707
Cade Daniel <cade@anyscale.com>
2022-09-07 13:40:40 -0700

    Simple cache for ray nightly wheels

---
+++ ray-release-test-wheel-cache/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -SO https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-3.0.0.dev0-cp37-cp37m-macosx_10_15_intel.whl
+curl -SO https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-3.0.0.dev0-cp37-cp37m-manylinux2014_x86_64.whl
