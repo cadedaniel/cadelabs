d44e06f32d1b09b29b85d5f7a2986dbbfefc8492
Cade Daniel <cade@anyscale.com>
2022-11-01 12:24:45 -0700

    ray powershell fix

--- ray-powershell-fix/test.sh
+++ ray-powershell-fix/test.sh
@@ -2,7 +2,7 @@
 
 pwsh=$(which pwsh)
 
-export COMPLEX_ENV_VAR_1=$(cat << EOF
+export COMPLEX_ENV_VAR_1=$(cat <<- "EOF"
 [Jobs] Deflake jobs sdk test (#29707)
 --
 Wait for all nodes to come up before running the unit test to deflake the unit test. It submits a bunch of jobs and checks that at least one was run on a worker node. However if the jobs are submitted too soon, its' possible that only the head node has come up, so 'theyll all be scheduled on the head node. The fix is to only submit the jobs after all the nodes are up.
@@ -12,5 +12,7 @@ Related issue number
 Closes #29006
 EOF)
 
-#env -i COMPLEX_ENV_VAR_1="$COMPLEX_ENV_VAR_1" $pwsh ps.ps
-$pwsh ps.ps
+env -i COMPLEX_ENV_VAR_1="$COMPLEX_ENV_VAR_1" $pwsh ps.ps | sed 's/export PATH/export FAKE_PATH/g' > refreshenv.sh
+echo 'env' >> refreshenv.sh
+
+env -i bash refreshenv.sh
