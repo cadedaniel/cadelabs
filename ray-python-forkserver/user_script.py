951a6610f43d131b5e3147230008cc94367dc3de
Cade Daniel <edacih@gmail.com>
2023-01-14 15:48:33 -0800

    WIP

---
+++ ray-python-forkserver/user_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import slow_import
+import os
+import time
+import sys
+
+print(f'User script loaded, pid={os.getpid()}. Sleeping forever.')
+sys.stdout.flush()
+
+while True:
+    time.sleep(1)
