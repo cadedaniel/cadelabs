bf930a7fa2db0ce5747f22987e2bf7141056656f
Cade Daniel <edacih@gmail.com>
2023-03-09 14:24:49 -0800

    wip

--- ray-pt-dataloader-fork-issue/script.py
+++ ray-pt-dataloader-fork-issue/script.py
@@ -3,13 +3,21 @@
 import ray
 import time
 import os
+import torch
 
-@ray.remote
+import subprocess
+print('killing previous')
+subprocess.run('ps aux | grep Fork | grep Actor | awk \'{print $2}\' | xargs kill -9', shell=True)
+
+@ray.remote(num_gpus=1)
 class ForkActor:
     def __init__(self):
         pass
 
     def fork(self):
+        print('creating torch cuda tensor')
+        torch.zeros(5).cuda()
+
         print('forking')
         pid = os.fork()
         print('fork done')
@@ -20,15 +28,19 @@ class ForkActor:
 
     def do_child(self):
         while True:
-            with open('~/workspace-project-cade-dev/ray-pt-dataloader-fork-issue/log', 'a') as f:
+            with open('/home/ray/workspace-project-cade-dev/ray-pt-dataloader-fork-issue/log', 'a') as f:
                 print(f'child pid {os.getpid()} parent {os.getppid()}', file=f)
             time.sleep(1)
 
     def do_parent(self):
         while True:
-            with open('~/workspace-project-cade-dev/ray-pt-dataloader-fork-issue/log', 'a') as f:
+            with open('/home/ray/workspace-project-cade-dev/ray-pt-dataloader-fork-issue/log', 'a') as f:
                 print(f'parent pid {os.getpid()} parent {os.getppid()}', file=f)
             time.sleep(1)
 
 a = ForkActor.remote()
 ray.get(a.fork.remote())
+
+while True:
+    print('kill')
+    time.sleep(1)
