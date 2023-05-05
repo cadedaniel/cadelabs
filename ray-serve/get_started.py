7907792712408d0fa902ad83629a8fa5003fa2d0
Cade Daniel <cade@anyscale.com>
2023-05-04 23:51:01 -0700

    asd

--- ray-serve/get_started.py
+++ ray-serve/get_started.py
@@ -40,51 +40,68 @@ volume = SharedVolume().persist("stable-diff-model-vol-2")
     ),
     shared_volumes={CACHE_PATH: volume},
     #secret=Secret.from_name("huggingface-secret"),
+    timeout=1200,
 )
 async def run_stable_diffusion():
-    import time
-    start_time = time.time()
-    import torch
-    from accelerate import init_empty_weights, load_checkpoint_and_dispatch, infer_auto_device_map, dispatch_model
-    from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
-    from transformers.generation.logits_process import LogitsProcessorList, LogitNormalization
-    print(f'imports done {time.time()-start_time:.02f}')
+
+    import os
+    os.environ['TRANSFORMERS_CACHE'] = CACHE_PATH
 
     model_to_use = "cerebras/Cerebras-GPT-111M"
     #model_to_use = "cerebras/Cerebras-GPT-6.7B"
     #model_to_use = "EleutherAI/gpt-neo-125m"
     #model_to_use = "facebook/opt-13b"
 
-    config = AutoConfig.from_pretrained(model_to_use, cache_dir=CACHE_PATH)
-    print(f'config loaded {time.time()-start_time:.02f}')
-    model = AutoModelForCausalLM.from_config(config)
-    print(f'cpu model instantiated {time.time()-start_time:.02f}')
-    model = model.eval()
-    print(f'eval mode set {time.time()-start_time:.02f}')
-    model = model.to(torch.float16)
-    print(f'fp16 set {time.time()-start_time:.02f}')
-    model = model.to('cuda:0')
-    print(f'sent to gpu {time.time()-start_time:.02f}')
+    from transformers import pipeline
+    #generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+    generator = pipeline('text-generation', model=model_to_use, device='cuda:0')
+    for _ in range(5):
+        out = generator("The quick brown fox jumps over", do_sample=True, min_length=50, max_length=50)
+        print(out)
 
-    print(f'memory available {torch.cuda.get_device_properties(0).total_memory / 2**30:.02f}')
-    
-    batch_size = 1
-    context_length = 512
-    device = 'cuda:0'
-    vocab_size = 50257
+    #import time
+    #start_time = time.time()
+    #import torch
+    #from accelerate import init_empty_weights, load_checkpoint_and_dispatch, infer_auto_device_map, dispatch_model
+    #from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+    #from transformers.generation.logits_process import LogitsProcessorList, LogitNormalization
+    #print(f'imports done {time.time()-start_time:.02f}')
 
-    #input_ids = (torch.rand(batch_size, context_length, device=device) * vocab_size).to(torch.int64)
+    ##model_to_use = "cerebras/Cerebras-GPT-111M"
+    ##model_to_use = "cerebras/Cerebras-GPT-6.7B"
+    ##model_to_use = "EleutherAI/gpt-neo-125m"
+    #model_to_use = "facebook/opt-13b"
+
+    #config = AutoConfig.from_pretrained(model_to_use, cache_dir=CACHE_PATH)
+    #print(f'config loaded {time.time()-start_time:.02f}')
+    #model = AutoModelForCausalLM.from_config(config)
+    #print(f'cpu model instantiated {time.time()-start_time:.02f}')
+    #model = model.eval()
+    #print(f'eval mode set {time.time()-start_time:.02f}')
+    #model = model.to(torch.float16)
+    #print(f'fp16 set {time.time()-start_time:.02f}')
+    #model = model.to('cuda:0')
+    #print(f'sent to gpu {time.time()-start_time:.02f}')
+
+    #print(f'memory available {torch.cuda.get_device_properties(0).total_memory / 2**30:.02f}')
+    #
+    #batch_size = 1
+    #context_length = 512
+    #device = 'cuda:0'
+    #vocab_size = 50257
+
+    ##input_ids = (torch.rand(batch_size, context_length, device=device) * vocab_size).to(torch.int64)
 
 
-    from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
-    tokenizer = AutoTokenizer.from_pretrained(model_to_use, cache_dir=CACHE_PATH)
-    inputs = tokenizer("The quick brown fox jumped over the lazy", return_tensors="pt")
-    #inputs = {k: v.to(device) for k, v in inputs.items()}
-    inputs = inputs['input_ids'].to(device)
-    print(inputs)
-    outputs = model.generate(inputs, max_new_tokens=5, pad_token_id=tokenizer.eos_token_id, do_sample=True)
-    print(outputs)
-    print(tokenizer.batch_decode(outputs))
+    #from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
+    #tokenizer = AutoTokenizer.from_pretrained(model_to_use, cache_dir=CACHE_PATH)
+    #inputs = tokenizer("Hello, I'm am conscious and", return_tensors="pt")
+    ##inputs = {k: v.to(device) for k, v in inputs.items()}
+    #inputs = inputs['input_ids'].to(device)
+    #print(inputs)
+    #outputs = model.generate(inputs, min_length=50, max_length=50, do_sample=True)
+    #print(outputs)
+    #print(tokenizer.batch_decode(outputs))
 
     #lm_logits = model.forward(
     #    input_ids,
